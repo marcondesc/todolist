7231e8a277fd0afb131c476f1b82cd8c56ef5233
Marcondes Cunha <marcondesc@gmail.com>
2022-11-06 11:05:32 -0300

    testando e treinando if/else e laços de repetição dentro das views.

--- Controllers/TestController.cs
+++ Controllers/TestController.cs
@@ -29,7 +29,27 @@ public class TestController : Controller
 
     public IActionResult ClientDetails()
     {
-        var client = new Client { Name = "Marcondes Cunha", Email = "marcondesc@gmail.com" };
+        var client = new Client
+        {
+            Name = "Marcondes Cunha",
+            Email = "marcondesc@gmail.com",
+            Purchases = 70,
+            Type = 3
+        };
         return View(client);
     }
+
+    public IActionResult ClientList()
+    {
+        var clients = new List<Client>();
+
+        clients.Add(new Client { Name = "Marcondes Cunha", Email = "marcondesc@gmail.com", Purchases = 170, Type = 3 });
+        clients.Add(new Client { Name = "John Doe", Email = "doe.john@mail.com", Purchases = 2, Type = 1 });
+        clients.Add(new Client { Name = "Jane Doe", Email = "jane.doe@mail.com", Purchases = 52, Type = 2 });
+
+        return View(clients);
+    }
+
+
+
 }
--- Models/Client.cs
+++ Models/Client.cs
@@ -4,4 +4,6 @@ public class Client
 {
     public string Name { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
+    public uint Purchases { get; set; }
+    public uint Type { get; set; }
 }
