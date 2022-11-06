b7163a21f2857f3771c1a7e2a3a1b9ba22e08683
Marcondes Cunha <marcondesc@gmail.com>
2022-11-06 10:30:19 -0300

    acrescentando exemplos de viwdata, viewbag e tempdata, treinando e testando views tipadas.

--- Controllers/TestController.cs
+++ Controllers/TestController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using TWTodoList.Models;
 
 namespace TWTodoList.Controllers;
 
@@ -6,8 +7,29 @@ public class TestController : Controller
 {
     public IActionResult Index()
     {
+        var todo = new Todo("Estudar Razor", DateTime.Now);
+        var todo2 = new Todo("Finalizar curso de Razor logo", DateTime.Now);
+        ViewData["todo"] = todo;
+        ViewData["name2"] = "Marcondes Cunha";
+
+        TempData["name"] = "mcws";
+
+        //viewbag
+        ViewBag.Name = "É ... um grande xablau!";
+        ViewBag.Todo = todo2;
+
+
         return View();
     }
 
+    public IActionResult AnotherTest()
+    {
+        return View();
+    }
 
+    public IActionResult ClientDetails()
+    {
+        var client = new Client { Name = "Marcondes Cunha", Email = "marcondesc@gmail.com" };
+        return View(client);
+    }
 }
---
+++ Models/Client.cs
@@ -0,0 +1,7 @@
+namespace TWTodoList.Models;
+
+public class Client
+{
+    public string Name { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+}
